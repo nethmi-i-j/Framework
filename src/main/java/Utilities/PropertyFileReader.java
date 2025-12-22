d050b236f1040491e3aa7a74041d68736726f21c
Nethmi <nethmijayasinghe31@gmail.com>
2025-12-22 16:10:14 +0530

    To read property files.

---
+++ src/main/java/Utilities/PropertyFileReader.java
@@ -0,0 +1,48 @@
+package Utilities;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyFileReader {
+
+    private static PropertyFileReader propReader;
+
+    // Implement singleton pattern
+    public static PropertyFileReader getInstance() {
+        if (propReader == null) {
+            propReader = new PropertyFileReader();
+            System.out.println("Creating new object");
+            return propReader;
+        } else {
+            System.out.println("Using existing object");
+            return propReader;
+        }
+    }
+
+    private Properties getData(String fileName) {
+        File file = new File(System.getProperty("user.dir")+"\\src\\test\\resources\\" + fileName + ".properties");
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Properties prop = new Properties();
+        try {
+            prop.load(fileInputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return prop;
+    }
+
+    public String getProperty(String fileName, String key) {
+        return getData(fileName).getProperty(key);
+    }
+
+
+}
