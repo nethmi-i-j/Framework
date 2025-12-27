d6382e01e8d5ddd14fa6adbcf14ee6932649690d
Nethmi <nethmijayasinghe31@gmail.com>
2025-12-27 15:42:09 +0530

    Added @BeforeSuite and @AfterSuite with including ExtentReportUtility

--- src/main/java/Base/BaseClass.java
+++ src/main/java/Base/BaseClass.java
@@ -1,6 +1,8 @@
 package Base;
 
+import Utilities.ExtentReporter;
 import Utilities.PropertyFileReader;
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
@@ -8,8 +10,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 public class BaseClass {
@@ -17,6 +23,28 @@ public class BaseClass {
     private static final Logger logger = LogManager.getLogger(BaseClass.class);
 
     public static WebDriver driver;
+
+
+    @BeforeSuite
+    public void beforeSuite(){
+
+        ExtentReporter.startReporter();
+
+        String logFilePath = "logs/Automation.log";
+        File logFile = new File(logFilePath);
+
+        try {
+            if (logFile.exists()) {
+                FileUtils.write(logFile, "", false);
+                logger.info("Log file cleaned: " + logFilePath);
+            } else {
+                FileUtils.touch(logFile);
+                logger.info("Log file created hence not exist: " + logFilePath);
+            }
+        } catch (IOException e) {
+            logger.info("error cleaning or creating the log file: " + e.getMessage());
+        }
+    }
 
     @BeforeMethod
     public void openPage(){
@@ -46,5 +74,12 @@ public class BaseClass {
     @AfterMethod
     public void closeBrowser(){
         driver.quit();
+    }
+
+
+    @AfterSuite
+    public void afterSuite(){
+        ExtentReporter.endReport();
+
     }
 }
