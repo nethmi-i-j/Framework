4960f70d99c95e004ba7be6941398d9c163f4970
Nethmi <nethmijayasinghe31@gmail.com>
2026-01-07 11:36:21 +0530

    Implemented IAnnotationTransformer Interface to the class and override the transform method.

--- src/main/java/Utilities/ListenerManager.java
+++ src/main/java/Utilities/ListenerManager.java
@@ -4,10 +4,17 @@ import Base.BaseClass;
 import org.testng.IAnnotationTransformer;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import org.testng.annotations.ITestAnnotation;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 public class ListenerManager extends BaseClass implements ITestListener, IAnnotationTransformer {
 
-
+    @Override
+    public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
+        annotation.setRetryAnalyzer(RetryAnalyzer.class);
+    }
 
     // implements unimplemented methods belongs to the ITestListener interface, then used @Override
     @Override
